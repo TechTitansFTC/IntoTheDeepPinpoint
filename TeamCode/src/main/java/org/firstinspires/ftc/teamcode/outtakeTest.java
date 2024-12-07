cb1392ef22a029b486ed92fadc73e6d702d3ee09
midK-night <pasum462@gmail.com>
2024-12-06 20:12:34 -0600

    update outtake test
    allows us to do shoulders and elbows (respectivelyy) together

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outtakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outtakeTest.java
@@ -14,33 +14,76 @@ public class outtakeTest extends LinearOpMode {
         Servo elbowR = hardwareMap.get(Servo.class, "elbowR");
         Servo wrist = hardwareMap.get(Servo.class, "wrist"); // L-R
         Servo claw = hardwareMap.get(Servo.class, "claw");
-        Servo[] servos = {shoulderL, shoulderR, elbowL, elbowR, wrist, claw};
         ElapsedTime timer = new ElapsedTime();
+        String[] servos = {"Shoulders", "Elbows", "Wrist", "Claw"};
 
         waitForStart();
         if (isStopRequested()) {return;}
         timer.reset();
         int testServoNum = 0;
         while (opModeIsActive()) {
-            Servo temp = servos[testServoNum];
+            // 0 - shoulders, 1 - elbows, 2 - wrist, 3 - claw
+            // need to check whether l and r will move in same way - i coded in a way it should work
+
             if (timer.milliseconds() > 300) {
-                if (gamepad1.dpad_up) {
-                    temp.setPosition(temp.getPosition() + 0.05);
-                } else if (gamepad1.dpad_down) {
-                    temp.setPosition(temp.getPosition() - 0.05);
-                } else if (gamepad1.dpad_right) {
-                    temp.setPosition(temp.getPosition() + 0.01);
-                } else if (gamepad1.dpad_left) {
-                    temp.setPosition(temp.getPosition() - 0.01);
+                switch (testServoNum) {
+                    case 0:
+                        if (gamepad1.dpad_up) {
+                            shoulderL.setPosition(shoulderL.getPosition() + 0.05);
+                            shoulderR.setPosition(shoulderR.getPosition() - 0.05);
+                        } else if (gamepad1.dpad_down) {
+                            shoulderL.setPosition(shoulderL.getPosition() - 0.05);
+                            shoulderR.setPosition(shoulderR.getPosition() + 0.05);
+                        } else if (gamepad1.dpad_right) {
+                            shoulderL.setPosition(shoulderL.getPosition() + 0.01);
+                            shoulderR.setPosition(shoulderR.getPosition() - 0.01);
+                        } else if (gamepad1.dpad_left) {
+                            shoulderL.setPosition(shoulderL.getPosition() - 0.01);
+                            shoulderR.setPosition(shoulderR.getPosition() + 0.01);
+                        }
+                        break;
+                    case 1:
+                        if (gamepad1.dpad_up) {
+                            elbowL.setPosition(elbowL.getPosition() + 0.05);
+                            elbowR.setPosition(elbowR.getPosition() - 0.05);
+                        } else if (gamepad1.dpad_down) {
+                            elbowL.setPosition(elbowL.getPosition() - 0.05);
+                            elbowR.setPosition(elbowR.getPosition() + 0.05);
+                        } else if (gamepad1.dpad_right) {
+                            elbowL.setPosition(elbowL.getPosition() + 0.01);
+                            elbowR.setPosition(elbowR.getPosition() - 0.01);
+                        } else if (gamepad1.dpad_left) {
+                            elbowL.setPosition(elbowL.getPosition() - 0.01);
+                            elbowR.setPosition(elbowR.getPosition() + 0.01);
+                        }
+                        break;
+                    case 2:
+                        if (gamepad1.dpad_up) {
+                            wrist.setPosition(wrist.getPosition() + 0.05);
+                        } else if (gamepad1.dpad_down) {
+                            wrist.setPosition(wrist.getPosition() - 0.05);
+                        } else if (gamepad1.dpad_right) {
+                            wrist.setPosition(wrist.getPosition() + 0.01);
+                        } else if (gamepad1.dpad_left) {
+                            wrist.setPosition(wrist.getPosition() - 0.01);
+                        }
+                        break;
+                    case 3:
+                        if (gamepad1.dpad_up) {
+                            claw.setPosition(claw.getPosition() + 0.05);
+                        } else if (gamepad1.dpad_down) {
+                            claw.setPosition(claw.getPosition() - 0.05);
+                        } else if (gamepad1.dpad_right) {
+                            claw.setPosition(claw.getPosition() + 0.01);
+                        } else if (gamepad1.dpad_left) {
+                            claw.setPosition(claw.getPosition() - 0.01);
+                        }
+                        break;
                 }
                 timer.reset();
             }
 
-            if (gamepad1.right_bumper) {
-                testServoNum += 1;
-            } else if (gamepad1.left_bumper) {
-                testServoNum -= 1;
-            }
+
 
             telemetry.addData("Testing Servo", servos[testServoNum]);
 
@@ -50,6 +93,11 @@ public class outtakeTest extends LinearOpMode {
             telemetry.addData("pos", wrist.getPosition());
             telemetry.addData("pos", claw.getPosition());
             telemetry.update();
+            if (gamepad1.right_bumper) {
+                testServoNum += 1;
+            } else if (gamepad1.left_bumper) {
+                testServoNum -= 1;
+            }
             if (testServoNum >= servos.length) {
                 testServoNum = 0;
             }
